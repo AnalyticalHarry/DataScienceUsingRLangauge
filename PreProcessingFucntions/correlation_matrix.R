a6dbed70954823cf6c9c5fabd43c0c0757729fa9
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-02-20 02:35:03 +0000

    Update correlation_matrix.R

--- PreProcessingFucntions/correlation_matrix.R
+++ PreProcessingFucntions/correlation_matrix.R
@@ -1,19 +1,17 @@
-install.packages("ggplot2")
-install.packages("tidyr")
+library(ggplot2)
+library(reshape2) 
 
+# function to create a correlation matrix plot
 correlation_matrix <- function(corr_matrix, threshold = 0.7) {
-  library(ggplot2)
-  library(tidyr)
-  
   # reshape correlation matrix for plotting
-  corr_matrix_melted <- melt(corr_matrix)
+  corr_matrix_melted <- melt(corr_matrix)  # Use melt from reshape2 package
   
   # heatmap
   ggplot(data = corr_matrix_melted, aes(Var2, Var1, fill = value)) +
     geom_tile() +
     geom_text(aes(label = round(value, 2)), vjust = 1) +
     scale_fill_gradient2(low = "blue", high = "red", mid = "gray",
-                         midpoint = 0, limit = c(-1,1), na.value = "grey50") +
+                         midpoint = 0, limit = c(-1, 1), na.value = "grey50") +
     theme_minimal() +
     coord_equal() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
@@ -26,4 +24,9 @@ correlation_matrix <- function(corr_matrix, threshold = 0.7) {
     theme(plot.title = element_text(hjust = 0.5))
 }
 
-# correlation_matrix(corr_matrix)
+# calcualte correlation 
+corr_matrix <- cor(data)
+
+# plot the correlation matrix
+correlation_matrix(corr_matrix)
+
